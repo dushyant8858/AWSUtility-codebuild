cdb41eabe0bd897656456782c9ea1f23e9e27d4c
Dushyant Dubaria <dushyant.dubaria2010@gmail.com>
2020-05-15 03:11:43 -0500

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,15 +1,6 @@
-# echo Switching to Root user...
-# sudo su
+cat docker-build.sh
 echo Updating the image on `date`...
 sudo yum update -y
-
-# echo Updating the AWS CLI started on `date`...
-# sudo yum install python3-pip -y
-# pip3 install awscli --user
-# python --version 
-# python3 --version 
-# pip3 --version
-# aws --version 
 
 echo Installing Docker on `date`...
 sudo yum install docker -y 
@@ -24,21 +15,17 @@ git clone https://github.com/dushyant8858/AWSUtility-codebuild.git
 cd AWSUtility-codebuild
 
 echo Setting build environment variable on `date`...
-# REPOSITORY_URI=821438830100.dkr.ecr.us-east-1.amazonaws.com/custom-docker-imagetag
-REPOSITORY_URI=dushyant8858/custom-docker-imagetag
+REPOSITORY_URI=821438830100.dkr.ecr.us-east-1.amazonaws.com/custom-docker-imagetag
 COMMIT_HASH=$(git rev-parse HEAD | cut -c 1-7)
 IMAGE_TAG=${COMMIT_HASH:=latest}
 
-# echo Logging in to Amazon ECR...
-# $(aws ecr get-login --no-include-email --region us-east-1)
+echo Logging in to Amazon ECR...
+$(aws ecr get-login --no-include-email --region us-east-1)
 
 echo Building the Docker image, started at `date`...          
-# docker build -t $REPOSITORY_URI:latest .
-# docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$IMAGE_TAG
 sudo docker build -t $REPOSITORY_URI:$IMAGE_TAG .
 
-echo Pushing the Docker image to Docker Hub, started at `date`...
-# docker push $REPOSITORY_URI:latest
+echo Pushing the Docker image to ECR, started at `date`...
 sudo docker push $REPOSITORY_URI:$IMAGE_TAG
 echo Docker image build and push done completed on `date`
 
@@ -48,8 +35,6 @@ echo Updating SSM imageTag parameter done on `date`...
 
 echo docker-build.sh execution done on `date`.
 
-
 #aws ssm get-parameter --name imageTag --region us-east-1 --query Parameter.Value --output text > /command-output.txt
-
 # wget -O - https://raw.githubusercontent.com/dushyant8858/AWSUtility-codebuild/master/docker-build.sh | bash
 echo -END-
