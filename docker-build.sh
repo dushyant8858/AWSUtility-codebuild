b02b0b32b46979ba45cb60b4c487095afab807e7
Dushyant Dubaria <dushyant.dubaria2010@gmail.com>
2020-05-15 02:28:46 -0500

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,10 +1,10 @@
-echo Switching to Root user...
-sudo su
+# echo Switching to Root user...
+# sudo su
 echo Updating the image on `date`...
-yum update -y
+sudo yum update -y
 
 echo Updating the AWS CLI started on `date`...
-yum install python3-pip -y
+sudo yum install python3-pip -y
 pip3 install awscli
 python --version 
 python3 --version 
@@ -12,12 +12,12 @@ pip3 --version
 aws --version 
 
 echo Installing Docker on `date`...
-yum install docker -y 
-docker --version
-systemctl start docker
+sudo yum install docker -y 
+sudo docker --version
+sudo systemctl start docker
 
 echo Installing git on `date`...
-yum install git -y 
+sudo yum install git -y 
 git --version
 
 git clone https://github.com/dushyant8858/AWSUtility-codebuild.git
@@ -29,16 +29,16 @@ COMMIT_HASH=$(git rev-parse HEAD | cut -c 1-7)
 IMAGE_TAG=${COMMIT_HASH:=latest}
 
 echo Logging in to Amazon ECR...
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 821438830100.dkr.ecr.us-east-1.amazonaws.com
+$(aws ecr get-login --no-include-email --region us-east-1)
 
 echo Building the Docker image, started at `date`...          
 # docker build -t $REPOSITORY_URI:latest .
 # docker tag $REPOSITORY_URI:latest $REPOSITORY_URI:$IMAGE_TAG
-docker build -t $REPOSITORY_URI:$IMAGE_TAG .
+sudo docker build -t $REPOSITORY_URI:$IMAGE_TAG .
 
 echo Pushing the Docker image to ECR, started at `date`...
 # docker push $REPOSITORY_URI:latest
-docker push $REPOSITORY_URI:$IMAGE_TAG
+sudo docker push $REPOSITORY_URI:$IMAGE_TAG
 echo Docker image build and push done completed on `date`
 
 echo Updating the SSM imageTag parameter...
